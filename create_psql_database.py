68c118f5861e5c26d4b5d7eea47fe4210af04ff0
mcshakes <efmccarthy26@gmail.com>
2024-05-31 09:12:44 -0600

    Have it working and models are created in the DB container

--- create_psql_database.py
+++ create_psql_database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import MetaData
 from sqlalchemy.orm import sessionmaker
-from setup_psql_environment import get_database
+from setup_psql_env import get_database
 from sqlalchemy.ext.declarative import declarative_base
 
 import models
@@ -8,8 +8,10 @@ import models
 # Setup environment and create a session
 db = get_database()
 Session = sessionmaker(bind=db)
-meta = MetaData(bind=db)
 session = Session()
+
+meta = MetaData()
+meta.bind = db
 
 # Create database from SQLAlchemy models
 models.Base.metadata.create_all(db)
